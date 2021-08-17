da16d98bd36ce5c4cf9cda2ce4aaa56d87b9d3f5
Tailchakra <chrisvdgeld@gmail.com>
2021-08-17 15:49:17 +0200

    Changed to hash mode

--- src/router.js
+++ src/router.js
@@ -14,7 +14,7 @@ const router = new Router({
     // scrollBehavior(to, from, savedPosition) {
     //     return {x: 0, y: 0}
     // },
-    mode: "history",
+    mode: "hash",
     routes: [
         {
             path: '/',
